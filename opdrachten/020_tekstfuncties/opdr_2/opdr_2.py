573e30fc5c1ea8dd076e9d961bfadd1940db0cd8
Mootje39 <97063641@st.deltion.nl>
2025-03-20 14:37:11 +0100

    Opdracht 2 tekstfunctie 20-03-2025

--- opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
+++ opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
@@ -4,6 +4,12 @@
 
 # Hier komt je code...
 
-my_list = ...
+# Opdracht: Tel hoe vaak de letter "t" voorkomt in de string
 
-print()
+my_string = "Tinus gaat op zijn tandem naar de hottentottententoonstelling"
+
+t_count = my_string.lower().count('t') 
+
+print(f'De letter "t" komt {t_count}x voor in my_string')
+
+exit
