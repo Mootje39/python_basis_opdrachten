c11603dce018d23d99a265f548186e125002e3f4
Mootje39 <97063641@st.deltion.nl>
2025-03-27 13:47:05 +0100

    opdracht 1 berekeningen 27-03-2025

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -3,3 +3,17 @@
 # Groep:
 
 # Hier komt je code...
+
+# Vrienden en hun leeftijden
+ali = 25
+sam = 30
+max = 27
+
+# Berekeningen
+totale_leeftijd = ali + sam + max
+gemiddelde_leeftijd = totale_leeftijd / 3
+
+# Resultaat weergeven
+print("> Dit weten we over Ali, Sam en Max:")
+print(f"> De totale waarde van alle leeftijden bij elkaar opgeteld = {totale_leeftijd}")
+print(f"> De gemiddelde leeftijd van deze boys is: {gemiddelde_leeftijd:.0f}")
