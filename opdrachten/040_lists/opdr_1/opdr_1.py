4ffb7bc3d1752bc8e8170baac8fd5e5dc3cd2a53
Mootje39 <97063641@st.deltion.nl>
2025-03-27 14:11:47 +0100

    opdracht 1 lists 27-03-2025

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -7,3 +7,18 @@ dict_1 = ...
 dict_2 = ...
 dict_3 = ...
 dict_4 = ...
+
+# Lijst aanmaken
+personen = []
+
+# 4 dictionaries met gegevens van personen
+persoon1 = {"id": 0, "voornaam": "Emma", "achternaam": "Jansen"}
+persoon2 = {"id": 1, "voornaam": "Liam", "achternaam": "Bakker"}
+persoon3 = {"id": 2, "voornaam": "Sophie", "achternaam": "De Vries"}
+persoon4 = {"id": 3, "voornaam": "Noah", "achternaam": "Peters"}
+
+# Toevoegen van de dictionaries aan de lijst met een list-methode
+personen.extend([persoon1, persoon2, persoon3, persoon4])
+
+# Print de volledige naam van de 2e persoon
+print(personen[1]["voornaam"], personen[1]["achternaam"])
