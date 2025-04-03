c5e478a73eb43ec4d67a26ce73f79e1e303a1430
Mootje39 <97063641@st.deltion.nl>
2025-04-03 14:35:30 +0200

    Opdracht 3 Loops 03-04-25

--- opdrachten/060_loops/opdr_3/opdr_3.py
+++ opdrachten/060_loops/opdr_3/opdr_3.py
@@ -7,3 +7,16 @@
 # Hier start de for-loop
 
 my_list = []
+
+# Maak een lege lijst om de resultaten op te slaan
+resultaten = []
+
+# Gebruik een for-loop en range van 3 tot 81, met een stap van 3
+for i in range(3, 82, 3):
+    # Bereken het kwadraat van het getal en deel het door 3
+    resultaat = (i ** 2) / 3
+    # Voeg het resultaat toe aan de lijst
+    resultaten.append(resultaat)
+
+# Print de lijst met resultaten
+print(resultaten)
