c7bf7f84e055153d00e160dbef25fb3c3c612592
Mootje39 <97063641@st.deltion.nl>
2025-04-03 14:40:56 +0200

    opdracht 6 Loops 03-04-25

--- opdrachten/060_loops/opdr_6/opdr_6.py
+++ opdrachten/060_loops/opdr_6/opdr_6.py
@@ -7,3 +7,27 @@
 # Hier start de for-loop
 
 my_list = []
+
+# Stap 1: De oorspronkelijke lijst pizza's
+pizza_lijst = ['margharita', 'calzone', 'verdi', 'olivio', 'quattro stagioni']
+
+# Stap 2: Sorteer de lijst op alfabet
+pizza_lijst.sort()
+print(pizza_lijst)  # ['calzone', 'margharita', 'olivio', 'quattro stagioni', 'verdi']
+
+# Stap 3: Voeg een pizza naar keuze toe
+pizza_lijst.append('yo-favorito')
+print(pizza_lijst)  # ['calzone', 'margharita', 'olivio', 'quattro stagioni', 'verdi', 'yo-favorito']
+
+# Stap 4: Verwijder de pizza die je het minst lekker vindt (bijvoorbeeld 'olivio')
+pizza_lijst.remove('olivio')
+print(pizza_lijst)  # ['calzone', 'margharita', 'quattro stagioni', 'verdi', 'yo-favorito']
+
+# Stap 5: Print de eerste 3 pizza's
+print(pizza_lijst[:3])  # ['calzone', 'margharita', 'quattro stagioni']
+
+# Stap 6: Print alleen de middelste pizza uit de lijst
+print(pizza_lijst[len(pizza_lijst) // 2:len(pizza_lijst) // 2 + 1])  # ['quattro stagioni']
+
+# Stap 7: Print de laatste 3 pizza's
+print(pizza_lijst[-3:])  # ['quattro stagioni', 'verdi', 'yo-favorito']
