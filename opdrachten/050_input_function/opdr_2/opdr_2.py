a3aa9545243eaecd3c401151faccd5aac41e7fba
Mootje39 <97063641@st.deltion.nl>
2025-04-03 14:26:35 +0200

    Opdracht 2 Input_function 03-04-25

--- opdrachten/050_input_function/opdr_2/opdr_2.py
+++ opdrachten/050_input_function/opdr_2/opdr_2.py
@@ -5,3 +5,17 @@
 # Hier komt je code...
 
 gasten = ["Jij", ... ]
+
+# Stap 1: Maak de gastenlijst en voeg jezelf toe
+gasten = ["Paul", "Kees", "Marie", "Hilda"]
+gasten.insert(0, "Jij")  # Voeg jezelf als eerste toe
+print(gasten)  # ["Jij", "Paul", "Kees", "Marie", "Hilda"]
+
+# Stap 2: Marie gaat niet meer mee, verwijder haar uit de lijst
+gasten.remove("Marie")
+print(gasten)  # ["Jij", "Paul", "Kees", "Hilda"]
+
+# Stap 3: George wil mee en naast Kees zitten
+index_kees = gasten.index("Kees")  # Vind de positie van Kees
+gasten.insert(index_kees + 1, "George")  # Voeg George direct na Kees toe
+print(gasten)  # ["Jij", "Paul", "Kees", "George", "Hilda"]
