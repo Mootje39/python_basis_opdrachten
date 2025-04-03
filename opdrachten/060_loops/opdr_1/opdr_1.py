5c895ec83010a957d706d115d573b8f8de524d5d
Mootje39 <97063641@st.deltion.nl>
2025-04-03 14:31:10 +0200

    opdracht 1 Loops 03-04-25

--- opdrachten/060_loops/opdr_1/opdr_1.py
+++ opdrachten/060_loops/opdr_1/opdr_1.py
@@ -7,3 +7,13 @@
 # Hier start de for-loop....
 
 my_list = []
+
+# Maak een lege lijst
+getallen = []
+
+# Vul de lijst met getallen van 1 t/m 10 met een loop
+for i in range(1, 11):
+    getallen.append(i)
+
+# Print de lijst
+print(getallen)
