e5078ca69df5f243ada0c15980762b88ebbccc76
Mootje39 <97063641@st.deltion.nl>
2025-04-03 14:29:26 +0200

    Opdracht 3 Input_function 03-04-25

--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -4,3 +4,15 @@
 
 # Hier komt je code...
 
+# Vraag de gebruiker om een lijst met 5 objecten (bijv. steden, games, voertuigen)
+input_waarden = input("Voer minimaal 5 objecten in, gescheiden door een komma:\n> ")
+
+# Zet de input om in een lijst en verwijder eventuele spaties rondom de woorden
+objecten_lijst = [item.strip() for item in input_waarden.split(",")]
+
+# Sorteer de lijst in omgekeerde alfabetische volgorde
+objecten_lijst.sort(reverse=True)
+
+# Print de gesorteerde lijst
+print("\nGesorteerde lijst in omgekeerde volgorde:")
+print(objecten_lijst)
