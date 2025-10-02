55d0ef7acf63870e319c57455b162b41c5bc037a
Mootje39 <97063641@st.deltion.nl>
2025-10-02 15:47:29 +0200

    Opdracht 1 berekeningen 02-10-25

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -5,15 +5,15 @@
 # Hier komt je code...
 
 # Vrienden en hun leeftijden
-ali = 25
-sam = 30
-max = 27
+Bob = 15
+Joe = 23
+Fred = 22
 
 # Berekeningen
-totale_leeftijd = ali + sam + max
+totale_leeftijd = Bob + Joe + Fred
 gemiddelde_leeftijd = totale_leeftijd / 3
 
 # Resultaat weergeven
-print("> Dit weten we over Ali, Sam en Max:")
+print("> Dit weten we over Bob, Joe en Fred:")
 print(f"> De totale waarde van alle leeftijden bij elkaar opgeteld = {totale_leeftijd}")
 print(f"> De gemiddelde leeftijd van deze boys is: {gemiddelde_leeftijd:.0f}")
