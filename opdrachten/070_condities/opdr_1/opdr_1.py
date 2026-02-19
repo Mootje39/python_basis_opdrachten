42b87767a6e77fcccd9f83c28fb47218b26b6933
mootje39 <97063641@st.deltion.nl>
2026-02-19 15:08:56 +0100

    Opdracht 1 070 condities

--- opdrachten/070_condities/opdr_1/opdr_1.py
+++ opdrachten/070_condities/opdr_1/opdr_1.py
@@ -8,5 +8,16 @@
 
 my_list = []
 
-#for loop
-#if statement
+# for loop
+for i in range(3):
+    lengte = float(input("Geef een lengte van een zijde: "))
+    my_list.append(lengte)
+
+# if statement
+# Sorteer de lijst zodat de grootste waarde (hypotenusa) achteraan staat
+my_list.sort()
+
+if my_list[0]**2 + my_list[1]**2 == my_list[2]**2:
+    print("Dit is een rechthoekige driehoek.")
+else:
+    print("Dit is geen rechthoekige driehoek.")
