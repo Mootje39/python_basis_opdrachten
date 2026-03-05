3794d847a6ab37bbd287b8020de6704b8184d97b
Mootje39 <97063641@st.deltion.nl>
2026-03-05 15:48:15 +0100

    Opdracht 3 tekstbestanden 080 05-03-2026

--- opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
+++ opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
@@ -2,5 +2,36 @@
 # Naam student:
 # Groep:
 
+# opdr_3.py
 
+def encrypt(tekst):
+    resultaat = ""
+
+    for letter in tekst:
+        # Controleer of het een kleine letter is
+        if 'a' <= letter <= 'z':
+            nieuwe_code = ord(letter) + 5
+            if nieuwe_code > ord('z'):
+                nieuwe_code -= 26
+            resultaat += chr(nieuwe_code)
+
+        # Controleer of het een hoofdletter is
+        elif 'A' <= letter <= 'Z':
+            nieuwe_code = ord(letter) + 5
+            if nieuwe_code > ord('Z'):
+                nieuwe_code -= 26
+            resultaat += chr(nieuwe_code)
+
+        # Andere tekens blijven hetzelfde
+        else:
+            resultaat += letter
+
+    return resultaat
+
+
+# Hoofdprogramma
+tekst = input("Geef de tekst die wilt encrypten..\n")
+versleuteld = encrypt(tekst)
+
+print("\n" + versleuteld)
 
