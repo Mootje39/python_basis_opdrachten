eba4a238a8a63d27b25fbdd134c46a1df4c716e4
Mootje39 <97063641@st.deltion.nl>
2026-03-05 15:39:31 +0100

    Opdracht 1 tekstbestanden 080 05-03-2026

--- opdrachten/080_tekstbestanden/opdr_1/opdr_1.py
+++ opdrachten/080_tekstbestanden/opdr_1/opdr_1.py
@@ -1,5 +1,37 @@
 # Opdracht 1 while-loops
-# Naam student:
+# Naam student: Mohamed Maghri
 # Groep:
 
-# Jouw code komt hier
+# opdr_1.py
+# Enquete programma
+
+def stel_vragen():
+    print("Welkom bij de enquete!\n")
+
+    antwoord1 = input("1. Wat vind je van de huidige regering? ")
+    antwoord2 = input("2. Wat vind je van de Python-lessen tot nu toe? ")
+    antwoord3 = input("3. Wat vind jij de mooiste stad van Nederland? ")
+
+    return antwoord1, antwoord2, antwoord3
+
+
+def sla_resultaten_op(regering, python_lessen, stad):
+    with open("enquete_resultaten.txt", "a") as bestand:
+        bestand.write("Nieuwe enquete:\n")
+        bestand.write(f"Wat vind je van de huidige regering? {regering}\n")
+        bestand.write(f"Wat vind je van de Python-lessen tot nu toe? {python_lessen}\n")
+        bestand.write(f"Wat vind jij de mooiste stad van Nederland? {stad}\n")
+        bestand.write("-" * 40 + "\n")
+
+
+def main():
+    regering, python_lessen, stad = stel_vragen()
+    sla_resultaten_op(regering, python_lessen, stad)
+
+    print("\nBedankt voor het invullen van de enquete!")
+    print("De resultaten zijn opgeslagen in 'enquete_resultaten.txt'.")
+
+
+# Start het programma
+if __name__ == "__main__":
+    main()
