fa95c2800fbbe60f2dfdc7e25bda2a2d070d698b
Mootje39 <97063641@st.deltion.nl>
2026-03-05 15:45:27 +0100

    Opdracht 2 tekstbestanden 080 05-03-2026

--- opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
+++ opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
@@ -3,8 +3,23 @@
 # Groep:
 
 import random
+
 prompt = "Raad mijn geheime getal \n"
 geheim_getal = random.randint(1, 100)
 
-# De rest moet jij doen.....
+aantal_pogingen = 0
+geraden = False
 
+while not geraden:
+    print(prompt)
+    gok = int(input())
+    aantal_pogingen += 1
+
+    if gok < geheim_getal:
+        print("hoger")
+    elif gok > geheim_getal:
+        print("lager")
+    else:
+        print(f"Je hebt het getal geraden het is {geheim_getal}!")
+        print(f"Je hebt het in {aantal_pogingen} keer gedaan")
+        geraden = True
