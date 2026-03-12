44121e22a6914415d0a6e62e4387e2519850c313
Mootje39 <97063641@st.deltion.nl>
2026-03-12 14:50:12 +0100

    Opdracht 3 functies 12-03-2026

--- opdrachten/090_functies/opdr_3/opdr_3.py
+++ opdrachten/090_functies/opdr_3/opdr_3.py
@@ -2,19 +2,25 @@
 # Naam student:
 # Groep:
 
+# opdr_3.py
+import math  # Nodig voor pi
 
-def kubus_vol(m):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+# Functie: volume van een kubus
+def kubus_vol(zijde):
+    return zijde ** 3  # volume = zijde^3
 
-def bol_vol(r):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+# Functie: volume van een bol
+def bol_vol(radius):
+    return (4/3) * math.pi * (radius ** 3)  # volume = 4/3 * pi * r^3
 
-zijde = 5
-radius = 4
+# Voorbeeldgebruik
+kubus = 25
+bol = 14
 
-print(kubus_vol(5))
-print(bol_vol(4))
+# Berekeningen
+kubus_volume = kubus_vol(kubus)
+bol_volume = bol_vol(bol)
+
+# Output
+print(f"De inhoud van deze kubus is: {kubus_volume}")
+print(f"De inhoud van deze bol is: {bol_volume}")
