e56f8a44866ad685e09557008ee6e34d2e1b874e
Mootje39 <97063641@st.deltion.nl>
2026-03-12 14:44:45 +0100

    Opdracht 2 functies 12-03-2026

--- opdrachten/090_functies/opdr_2/opdr_2.py
+++ opdrachten/090_functies/opdr_2/opdr_2.py
@@ -2,19 +2,25 @@
 # Naam student:
 # Groep:
 
+# opdr_2.py
 
+# Functie: kilometers naar miles
 def kilometers_naar_miles(km):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+    return km / 1.609344
 
+# Functie: miles naar kilometers
 def miles_naar_kilometers(miles):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+    return miles * 1.609344
 
-kilometers = 1223
-miles = 867
 
-print(kilometers_naar_miles(kilometers))
-print(miles_naar_kilometers(miles))
+# Voorbeeldgebruik
+kilometers = 15000
+miles = 2700
+
+# Berekeningen
+miles_resultaat = kilometers_naar_miles(kilometers)
+km_resultaat = miles_naar_kilometers(miles)
+
+# Output
+print(f"{kilometers} kilometers = {miles_resultaat} miles")
+print(f"{miles} miles = {km_resultaat} kilometers")
